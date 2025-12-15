0fce285ba9e2020f7c0de156494883c59987c57d
benzidane027 <benzidane027@yahoo.com>
2025-12-15 23:05:50 +0100

    up

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,32 @@
 #include "objectbox-model.h"
 #include "task.obx.hpp"
 
+
+const std::string DB_PATH = std::string(PROJECT_SOURCE_DIR).append("/data/mydb");
+
+
 using namespace std;
 
-int main() {
-    cout << "******** starting ********" << endl<< endl;
-    obx::Store store(create_obx_model());
+
+int main()
+{
+    cout << "******** starting ********" << endl
+         << endl;
+    OBX_model *model = create_obx_model();
+    
+    obx::Options options(model);
+    options.directory(DB_PATH);
+    obx::Store store(options);
 
     obx::Box<Task> box(store);
-    obx_id id = box.put({.text = "Buy milk"});  // Create
-    std::unique_ptr<Task> task = box.get(id); 
-     if (task) {
-        cout << task->id << endl;  // Read
+    obx_id id = box.put({.text = "Buy milk"}); // Create
+    std::unique_ptr<Task> task = box.get(id);
+    if (task)
+    {
+        cout << task->id << endl; // Read
     }
-    cout <<endl<< "******** ending ********" << endl<< endl;
+    cout << endl
+         << "******** ending ********" << endl
+         << endl;
     return 0;
 }
-
