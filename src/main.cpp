6a84c97476fce3afbfad80ebb50f4f93479b2a7a
benzidane027 <benzidan27@yahoo.com>
2025-12-16 16:53:13 +0100

    up

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,47 @@
 #include "objectbox-model.h"
 #include "task.obx.hpp"
 #include <ctime>
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
 
 const std::string DB_PATH = std::string(PROJECT_SOURCE_DIR).append("/data/database");
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
      cout << "******** starting ********" << endl
           << endl;
-     OBX_model *model = create_obx_model();
-     obx::Options OPTIONS(model);
-     OPTIONS.directory(DB_PATH);
-     obx::Store store(OPTIONS);
 
-     obx::Box<Task> box(store);
+     QApplication app(argc, argv);
+     QWidget window;                // Main window
+     
 
-     Task _task;
-     _task.title = "hello";
-     _task.description = "bla bla bla ...";
-     _task.date_created = (int64_t)std::time(nullptr);
+     window.show(); // Show the window
 
-     obx_id id = box.put(_task); // Create
+     // OBX_model *model = create_obx_model();
+     // obx::Options OPTIONS(model);
+     // OPTIONS.directory(DB_PATH);
+     // obx::Store store(OPTIONS);
 
-     std::vector<unique_ptr<Task>> data = box.getAll();
+     // obx::Box<Task> box(store);
 
-     for_each(data.begin(), data.end(), [](const std::unique_ptr<Task> &t)
-              { cout << "Task ID: " << t->id << ", Title: " << t->title << ", Description: " << t->description
-                     << ", Date Created: " << t->date_created << endl; });
+     // Task _task;
+     // _task.title = "hello";
+     // _task.description = "bla bla bla ...";
+     // _task.date_created = (int64_t)std::time(nullptr);
+
+     // obx_id id = box.put(_task); // Create
+
+     // std::vector<unique_ptr<Task>> data = box.getAll();
+
+     // for_each(data.begin(), data.end(), [](const std::unique_ptr<Task> &t)
+     //          { cout << "Task ID: " << t->id << ", Title: " << t->title << ", Description: " << t->description
+     //                 << ", Date Created: " << t->date_created << endl; });
 
      cout << endl
           << "******** ending ********" << endl
           << endl;
-     return 0;
+     return app.exec();
 }
